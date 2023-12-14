3c3f4c301ab02a59828ce2a95f1c8e1c6eabd123
arj <arj@workingdoge.com>
2023-12-14 14:50:59 -0600

    commit

---
+++ test/receipt.test.ts
@@ -0,0 +1,54 @@
+import { AccountWallet, AztecAddress, Fr, PXE, TxStatus, Wallet, computeMessageSecretHash, createAccount, createPXEClient, Contract } from "@aztec/aztec.js";
+import { TokenContract } from "@aztec/noir-contracts/types";
+import { ReceiptCreatorContract, ReceiptCreatorContractArtifact } from "../contracts/target/ReceiptCreator.js"
+
+const { PXE_URL = "http://localhost:8080" } = process.env
+
+describe('receipt contract', () => {
+
+  let pxe: PXE;
+  let deployer: AccountWallet;
+  let sender: AccountWallet;
+  let recipient: AccountWallet;
+  let token: Contract;
+  let receipt_creator: Contract;
+
+
+  beforeEach(async () => {
+
+    // setup
+    pxe = createPXEClient(PXE_URL);
+    deployer = await createAccount(pxe);
+    sender = await createAccount(pxe);
+    recipient = await createAccount(pxe);
+
+    token = await TokenContract.deploy(deployer, deployer.getCompleteAddress()).send().deployed()
+
+    console.log("token deployed")
+    console.log(token)
+
+    receipt_creator = await ReceiptCreatorContract.deploy(deployer).send().deployed();
+    console.log("receipt_creator deployed")
+    console.log(receipt_creator)
+
+    // const initialBalance = 20n;
+    // const secret: Fr = Fr.random();
+    // const secretHash = await computeMessageSecretHash(secret)
+    // const mint_receipt = await token.methods.mint_private(initialBalance, secretHash).send().wait();
+    //
+    // const storageSlot = new Fr(5)
+
+
+  }, 60000);
+
+  it('Check Contract Deployment', async () => {
+    expect(token.completeAddress).toBeDefined()
+    expect(receipt_creator.completeAddress).toBeDefined()
+  })
+
+  xit('Generate Receipt', async () => {
+    //   // distribute token to sender
+    //
+  })
+
+})
